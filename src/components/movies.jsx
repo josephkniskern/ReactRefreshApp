083af421d792bab35a0a53d91bf5379028783521
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-10-05 15:12:45 -0400

    new features

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService'
+import Like from './common/like';
 
 class Movies extends Component {
 
@@ -7,26 +8,59 @@ class Movies extends Component {
     movies: getMovies()
    }
 
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id)
+    this.setState({ movies })
+   }
+
+   handleLike = (movie) => {
+    const movies = [...this.state.movies]
+    const index = movies.indexOf(movie)
+    movies[index] = { ...movies[index] }
+    movies[index].liked = !movies[index].liked
+    this.setState({ movies })
+   }
+
   render() { 
+    if (this.state.movies === 0) return <p>There are no movies in the database</p>
+
     return (
+      <>
+      <p>Showing {this.state.movies.length} movies in the database</p>
       <table className="table">
         <thead>
           <tr>
             <th>Title</th>
-            <th></th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
             <th></th>
             <th></th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          {this.state.movies.map(movie =>
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Like liked={movie.liked} onClick={() => this.handleLike(movie)}/>
+            </td>
+            <td>
+              <button 
+                onClick={() => this.handleDelete(movie)} 
+                className="btn btn-danger btn-sm"
+                >
+                  Delete
+              </button>
+            </td>
           </tr>
+            )}
         </tbody>
       </table>
+      </>
 
     )
 
