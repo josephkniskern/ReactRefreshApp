a028b49cd8c9d94e46f8181095a522ca20a56306
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-10-06 13:44:23 -0400

    adding genre select

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import Pagination from './common/pagination'
 import { getMovies } from '../services/fakeMovieService'
+import { getGenres } from '../services/fakeGenreService'
 import Like from './common/like';
+import { paginate } from '../utils/paginate'
+import ListGroup from './common/listGroup';
 
 class Movies extends Component {
 
   state = { 
-    movies: getMovies()
+    movies: [],
+    genres: [],
+    currentPage: 1,
+    pageSize: 4
+   }
+
+   componentDidMount() {
+     this.setState({ movies: getMovies(), genres: getGenres() })
    }
 
   handleDelete = movie => {
@@ -21,50 +32,77 @@ class Movies extends Component {
     this.setState({ movies })
    }
 
+   handlePageChange = page => {
+    this.setState({ currentPage: page })
+   }
+
+   handleGenreSelect = genre => {
+
+   }
+
   render() { 
+    const { length: count } = this.state.movies
+    const { pageSize, currentPage, movies: allMovies } = this.state
+
     if (this.state.movies === 0) return <p>There are no movies in the database</p>
 
+    const movies = paginate(allMovies, currentPage, pageSize)
+
     return (
-      <>
-      <p>Showing {this.state.movies.length} movies in the database</p>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map(movie =>
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <Like liked={movie.liked} onClick={() => this.handleLike(movie)}/>
-            </td>
-            <td>
-              <button 
-                onClick={() => this.handleDelete(movie)} 
-                className="btn btn-danger btn-sm"
-                >
-                  Delete
-              </button>
-            </td>
-          </tr>
-            )}
-        </tbody>
-      </table>
-      </>
-
-    )
-
-    
+      <div className="row">
+        <div className="col-3">
+          <ListGroup 
+            items={this.state.genres} 
+            textProperty="name"
+            valueProperty="_id"
+            onItemSelect={this.handleGenreSelect}
+          />
+        </div>
+        <div className="col">
+          <p>Showing {count} movies in the database</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map(movie =>
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like liked={movie.liked} onClick={() => this.handleLike(movie)}/>
+                </td>
+                <td>
+                  <button 
+                    onClick={() => this.handleDelete(movie)} 
+                    className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                  </button>
+                </td>
+              </tr>
+                )}
+            </tbody>
+          </table>
+          <Pagination 
+            itemsCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+            >
+          </Pagination>
+        </div>
+      </div>
+    )  
   }
 }
  
