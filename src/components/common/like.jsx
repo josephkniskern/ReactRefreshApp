2af7074414e92f4afe5288510e6383f76d1e7242
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-10-14 18:42:39 -0400

    cleaner with toastify

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React from "react";
 
 const Like = (props) => {
-  let classes = "fa fa-heart"
-  if (!props.liked) classes += "-o"
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
   return (
-    <i 
+    <i
       onClick={props.onClick}
       style={{ cursor: "pointer" }}
-      className={classes} 
+      className={classes}
       aria-hidden="true"
-      >
-    </i> 
-  )
-}
- 
+    ></i>
+  );
+};
+
 export default Like;
